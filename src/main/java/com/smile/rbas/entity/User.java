f5ac2500343feac57d007429d64ed19d2b2b12ae
JP007-Star <justice_of_peace@yahoo.com>
2021-11-18 00:15:52 +0530

    Api Based Setup

--- src/main/java/com/smile/rbas/entity/User.java
+++ src/main/java/com/smile/rbas/entity/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
@@ -15,7 +17,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name="USER_AUTH_TBL")
 public class User {
-
+    @Id
+    @GeneratedValue
     private int id;
     private String userName;
     private String password;
---
+++ src/main/java/com/smile/rbas/service/GroupUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.smile.rbas.service;
+
+import com.smile.rbas.entity.User;
+import com.smile.rbas.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class GroupUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user=repository.findByUserName(userName);
+        return user.map(GroupUserDetails::new).orElseThrow(()->new UsernameNotFoundException(userName+"doesn't exist"));
+    }
+}
